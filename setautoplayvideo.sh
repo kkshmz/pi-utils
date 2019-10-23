cf926906cde782ac656bf6857a7fb29e124ecb0f
kkshmz <kyeshimizu@gmail.com>
2019-10-23 22:00:47 +0900

    initial

--- setautoplayvideo.sh
+++ setautoplayvideo.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 #https://rusnak.io/autoplay-video-on-raspberry-pi/
 
 echo "/usr/bin/omxplayer -b --loop /home/pi/*.mp4" >> /etc/rc.local
 chmod +x /etc/rc.local
+# get rid of the cursor so we don't see it when videos are running
+setterm -cursor off
+
 sudo reboot
