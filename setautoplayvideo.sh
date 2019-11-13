621389908b96d7eb29b4ccdba4f47d7f87eab68e
kkshmz <kyeshimizu@gmail.com>
2019-11-13 22:10:53 +0900

    add anytokyo configs for raspi

--- setautoplayvideo.sh
+++ setautoplayvideo.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-#https://rusnak.io/autoplay-video-on-raspberry-pi/
-
-echo "/usr/bin/omxplayer -b --loop /home/pi/*.mp4" >> /etc/rc.local
-chmod +x /etc/rc.local
-# get rid of the cursor so we don't see it when videos are running
-setterm -cursor off
-
-sudo reboot
+# ADD this to bashrc with right video
+if [ $(tty) == /dev/tty1 ]; then
+omxplayer --blank --loop /home/pi/myvideo.mp4
+fi
